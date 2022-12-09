d386885ff92893e3e142e64db89c6c1a2e2f0fc1
Martina Garcia <martinagg@myuax.com>
2022-12-09 12:00:33 +0100

    apuntes

--- funciones.py
+++ funciones.py
@@ -30,21 +30,59 @@ nombre='martina'
 edad='22'
 print(f'mi nombre es {nombre} y tengo {edad} años')
 
-mi_lista=['martina',1,2,3,4,5,6,7,8,9,10]
-print(mi_lista)
-print(len(mi_lista))
-print(mi_lista[0])
-print(mi_lista[2:])
-primeralista=[1,2,3]
-segundalista=[4,5,6]
-lista=primeralista+segundalista
-lista[0]='mar'
+
+
+
+
+milista=[1,2,3]
+for num in range(10):  #va desde 0 hasta 9
+    print(num)
+print('------------------')
+for num in range(0,12,2): #va desde 0 hasta 11 de 2 en 2
+    print(num)
+print('------------------')
+print(list(range(0,20,5)))  #nos da una lista con los numeros que estan en el rango
+print('------------------')
+contador_indice=0
+palabra='hola'
+for letra in palabra:
+    print(palabra[contador_indice])
+    contador_indice+=1
+print('------------------')
+#otra manera de saber la posicion de cada letra
+for letra in enumerate(palabra):
+    print(letra)
+print('------------------')
+milista1=[1,2,3,]
+milista2=['a','b','c']
+for intem in zip(milista1,milista2):
+    print(intem)
+lista_nombres=['martina','jose','bea']
+lista_apellidos=['garcia','perez','gomez']
+for item in zip(lista_nombres, lista_apellidos):
+    print(item)
+print('------------------')
+lista_nombres=['martina','jose','bea']
+lista_apellidos=['garcia','perez','gomez']
+edades=[18,54,23]
+datos=list(zip(lista_nombres,lista_apellidos,edades))
+print(datos)
+print('------------------')
+#ahora imaginate que tenemos un diccionario y queremos saber si tenemos una clave
+diccionario={'a':1,'b':2,'c':3}
+if 1 in diccionario.values():
+    print('existe')
+print('------------------')
+from random import shuffle
+lista=[1,2,3,4,5,6,7,8,9,10]
+lista=shuffle(lista) #desordena la lista
+print('------------------')
+from random import randint
+print(randint(0,100)) #nos da un numero aleatorio entre 0 y 100 entero
+
+#nos permite crear una lista con las letras de un nombre
+minombre='martina'
+lista=[]
+for letra in minombre:
+    lista.append(letra)
 print(lista)
-lista.append(7)
-print(lista)
-intem_eliminado= lista.pop()
-print(intem_eliminado)
-print(lista)
-lista.pop(3)
-nuevalista1=print(lista)
-mi_nuevalista=['a','b','c']
